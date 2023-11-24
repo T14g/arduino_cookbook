5145af5003b3d3d5903b275a115a186ac106e841
T14g <ottiagos@gmail.com>
2023-11-23 23:51:40 -0300

    Pino 12 PB6 saída em baixo nível atmega2560

--- config_pin_exemple/config_pin_exemple.ino
+++ config_pin_exemple/config_pin_exemple.ino
@@ -2,8 +2,19 @@ void setup() {
   // Pino 12 = PB6
   // Pino 12 ATmega2560 como saída em nível alto, DDB6 = 1 e PORTB6 = 1
   // É possível configurar
- DDRB = DDRB | (1<<DDB6); // DDB6 = 1 , saída
- PORTB = PORTB | (1<<PB6); // PORTB6 = 1 , NÍVEL ALTO
+//  DDRB = DDRB | (1<<DDB6); // DDB6 = 1 , saída
+//  PORTB = PORTB | (1<<PB6); // PORTB6 = 1 , NÍVEL ALTO
+
+
+// ========================= //
+// Pino 12 PB6 como saída em nível baixo, DDB6 = 1 e PORTB6 = 0 
+DDRB = DDRB | (1<<DDB6); // DDB6 = 1 , saída
+PORTB = PORTB & ~ (1<<PB6); // PORTB6 = 0, nível baixo
+
+
+
+
+
 }
 
 void loop() {
