3b0d39bb234e13ea4eb6fa588054ff8818820ba8
T14g <ottiagos@gmail.com>
2023-11-27 21:51:44 -0300

    Pino 2 como entrada sem pull up

--- config_pin_exemple/config_pin_exemple.ino
+++ config_pin_exemple/config_pin_exemple.ino
@@ -14,10 +14,21 @@ void setup() {
 
 // =========================== // 
 // Pino 12 PB6 como entrada com Pull-Up DDB6=0 e PORTB6 = 1
-DDRB = DDRB & ~ (1<<DDB6); // DDB6=0 entrada
-PORTB = PORTB | (1<<PB6); // PORTB6 = 1 , com PULL-UP
+// DDRB = DDRB & ~ (1<<DDB6); // DDB6=0 entrada
+// PORTB = PORTB | (1<<PB6); // PORTB6 = 1 , com PULL-UP
 
+// ================================ //
 
+// Pino 12 PB6 ( pino B bit 6 ) como entrada sem pull-up ( alta impedância )
+// DDB6 = 0 e PORTB6 = 0
+// com Pull up fica sempre HIGH em um button e quando pressionado fica LOW
+// É bom para evitar falsas leituras
+// Reduz consumo de energia
+// Melhora a integridade do sinal ajudando a estabilizar a voltagem
+// Evita valores flutuantes, ex : flutuar entre HIGH e LOW
+
+DDRB = DDRB & ~ (1<<DDB6); // DDB6 = 0 , entrada
+PORTB = PORTB & ~ (1<<PB6); // PORTB6 = 0, sem pull - up
 }
 
 void loop() {
